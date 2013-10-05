34079c08194e5a8978405d87407fcf55104142cb
App Academy Student <student@appacademy.io>
2013-10-05 14:21:34 -0400

    Initializes app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormApp::Application.config.secret_token = '38c64439eac2bd9f68cbd4054e275ed8343e39f247f6e72ed4dc82cceeef0f1f86bb3eaad2854ce9b8bc6b5799b9fcb4003ef283171459a1e95407442f5e4550'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FormApp::Application.config.session_store :cookie_store, key: '_formApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FormApp::Application.config.session_store :active_record_store
