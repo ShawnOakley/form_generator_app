118ed71a20da81935125941a9568a2a3678214fe
App Academy Student <student@appacademy.io>
2013-10-07 14:03:59 -0400

    Implemented Form or Entry Model. Have not yet raked migrations.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :form_id, :type ,:name ,:id ,:accept ,:align ,:alt ,:autocomplete ,:disabled ,:formaction ,:formenctype ,:formmethod ,:formnovalidate ,:formtarget ,:height ,:list ,:max ,:min ,:pattern ,:placeholder ,:randomly ,:required ,:size ,:src ,:step ,:width ,:parsed_input
+
+  validates_presence_of :form_id, :type
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,5 @@
 class Form < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :owner_id, :form_name, :title, :captcha
+
+  validates_presence_of :owner_id, :form_name, :title, :captcha
 end
