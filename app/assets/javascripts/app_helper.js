a6e2305444a7f5b85cbfaa5e85f9097c364bcc96
App Academy Student <student@appacademy.io>
2013-10-08 22:29:10 -0400

    Partially implements toggling between input types.

--- app/assets/javascripts/app_helper.js
+++ app/assets/javascripts/app_helper.js
@@ -13,10 +13,21 @@ function addChoice(event) {
 };
 
 $(document).ready(function(){
+  $(document).find('.inp').children().toggleClass('hidden')
+  $(document).find(".select-button").first().click();
   $('.choice-collection').delegate('.delete-button', 'click', (function(){
     $(this).parent().remove();
-  })
-)});
+  }));
+
+  $('.input_field').delegate('.select-button', 'change', (function(){
+    console.log($(this).siblings().parent().siblings())
+    hidden_id = $(this).val();
+
+    console.log($(this).siblings().parent().siblings().find('.' + hidden_id))
+    $(this).siblings().parent().siblings().find('.' + hidden_id).toggleClass('hidden');
+  }));
+});
+
 
 
 
