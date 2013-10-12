833cc3d2968a0cb24596ec5aac5c6f59b514d60c
App Academy Student <student@appacademy.io>
2013-10-12 19:54:15 -0400

    removes information :

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
       :port                 => 587,
       :domain               => "gmail.com",
      :user_name            => "shawnoakley", #Your user name
-      :password             => "Goasoamichaux7283", # Your password
+      :password             => "", # Your password
       :authentication       => "plain",
       :enable_starttls_auto => true
    }
