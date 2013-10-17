b9d01e019d95e08b3010fb27aeb9f4a23fef6822
Shawn Oakley <shawnoakley@gmail.com>
2013-10-17 15:00:07 -0400

    Removes unique attribute on user email

--- db/migrate/20131005182742_create_users.rb
+++ db/migrate/20131005182742_create_users.rb
@@ -11,7 +11,7 @@ class CreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :username, :unique => true
-    add_index :users, :user_email, :unique => true
+    add_index :users, :user_email
     add_index :users, :session_token, :unique => true
   end
 end
